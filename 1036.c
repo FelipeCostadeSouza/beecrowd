62a1444ee5dd807097d09b54a71ac287752da13a
FelipeCostadeSouza <felipe.souza07@unifesp.br>
2025-07-02 10:56:46 -0300

    1036.c error

---
+++ 1036.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	double a, b, c;
+	scanf("%lf %lf %lf", &a, &b, &c);
+	double delta = (b*b)-4*a*c;
+	double raiz_de_delta = sqrt(delta);
+	double result1 = (-b+raiz_de_delta)/(2*a);
+	double result2 = (-b-raiz_de_delta)/(2*a);
+
+	if(a==0.0 || delta<0) {
+		printf("Impossivel calcular");
+	} else {
+		printf("R1 = %.5f\n", result1);
+		printf("R2 = %.5f\n", result2);
+	}
+	return 0;
+}

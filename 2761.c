3c528982625843628a9903773d87178cca86e212
FelipeCostadeSouza <felipe.souza07@unifesp.br>
2025-07-02 21:30:28 -0300

    Update 2761.c

--- 2761.c
+++ 2761.c
@@ -6,16 +6,17 @@ int main()
     int A;
     float B;
     char C;
-    char D[51];
-    
+    char D[49];
+
     scanf("%d %f %c", &A, &B, &C);
-    fgets(D,51,stdin);{
-       int i = strcspn(D, "\n");
-       D[i] = '\0';
+    getchar(); 
+    fgets(D, 49, stdin);
+    {
+        int i = strcspn(D, "\n");
+        D[i] = '\0';
     }
-    printf("%d%.6f%c%s", A, B, C, D);
+    printf("%d%.6f%c%s\n", A, B, C, D);
     printf("%d\t%.6f\t%c\t%s\n", A, B, C, D);
-    printf("%10d%10f%c%.10s", A, B, C, D);
-    
+    printf("%10d%10.6f%10c%10.10s\n", A, B, C, D);
     return 0;
 }

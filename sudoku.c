89f4cd0156b37505a8c7ae737922d16db9fac850
FelipeCostadeSouza <felipe.souza07@unifesp.br>
2025-07-04 15:03:17 -0300

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -2,7 +2,7 @@
 
 int main() {
   int i, j;
-
+  int n=0;
 
   
 
@@ -11,24 +11,43 @@ int main() {
   printf("Digite os elementos da matriz:\n");
   for (i = 0; i < 9; i++) {
     for (j = 0; j < 9; j++) {
-      printf("Elemento [%d][%d]: ", i, j);
-      scanf("%d", &matriz[i][j]);
       
+       
+       printf("Elemento [%d][%d]: ", i, j);
+       scanf("%d", matriz[i][j]);
+      }
       
       while (matriz[i][j] < 0 || matriz[i][j] > 9) {
         printf("Valor inválido. Digite novamente: ");
-        scanf("%d", &matriz[i][j]);
+        scanf("%d", matriz[i][j]);
       }
     }
   }
+  for(i=0 && j=0; matriz[i][j+n] != matriz[i][j+1]; j++){
+  
+   if(j<9){
+      
+      matriz[i][1] != matriz[i][j]
+      matriz[i][2] != matriz[i][j]
+      matriz[i][3] != matriz[i][j]
+      matriz[i][4] != matriz[i][j] 
+      matriz[i][5] != matriz[i][j]      
+      matriz[i][6] != matriz[i][j]
+      matriz[i][7] != matriz[i][j]
+      matriz[i][8] != matriz[i][j]
+   }else{
+    j=0;
+    i++;
+    }
+  }
 
-  for (i = 0; i < 9; i++) {
+for (i = 0; i < 9; i++) {
     for (j = 0; j < 9; j++) {
-      printf("%d ", matriz[i][j]);
+      printf("%d ", matriz[i][j]);    
     }
     printf("\n");
   }
-  if(matriz[][])
+  
 
 
 

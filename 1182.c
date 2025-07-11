1656bb62631e688b1777b8a7936163299cca7cb7
FelipeCostadeSouza <felipe.souza07@unifesp.br>
2025-07-11 20:08:31 -0300

    Create 1182.c

---
+++ 1182.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+	int L, i, j;
+	char Maiusculo;
+	scanf("%d", &L);
+	scanf(" %c", &Maiusculo);
+
+	double M[12][12];
+	for(int i=0; i<12 ; i++) {
+		for(int j=0; j<12; j++) {
+			scanf("%lf", &M[i][j]);
+		}
+
+	}
+	double soma = 0.0;
+	for (int i = 0; i < 12; i++) {
+		soma += M[i][L];
+	}
+	if(Maiusculo=='M') {
+		soma/=12;
+	}
+	printf("%.1lf\n", soma);
+
+	return	0;
+
+}

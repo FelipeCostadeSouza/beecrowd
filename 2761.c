ea40ac4ab05d5718e766d3c90ec685b1d013794f
FelipeCostadeSouza <felipe.souza07@unifesp.br>
2025-07-11 18:36:28 -0300

    Update 2761.c

--- 2761.c
+++ 2761.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
+int main() {
     int A;
     float B;
     char C;
-    char D[49];
+    char D[49];  
+    if (scanf("%d %f %c", &A, &B, &C) != 3) return 0;
+  
+    if (!fgets(D, sizeof D, stdin)) return 0;
+    D[strcspn(D, "\n")] = '\0';  
+    printf("%d %.6f %c %s\n", A, B, C, D);
 
-    scanf("%d %f %c", &A, &B, &C);
-    getchar(); 
-    fgets(D, 49, stdin);
-    {
-        int i = strcspn(D, "\n");
-        D[i] = '\0';
-    }
-    printf("%d%.6f%c%s\n", A, B, C, D);
     printf("%d\t%.6f\t%c\t%s\n", A, B, C, D);
-    printf("%10d%10.6f%10c%10.10s\n", A, B, C, D);
+
+    printf("%10d%10.6f%10c%10s\n", A, B, C, D);
+
     return 0;
 }

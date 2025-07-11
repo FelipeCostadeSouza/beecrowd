57f79c4bccaa13f959cf430daada1ed85529ff69
FelipeCostadeSouza <felipe.souza07@unifesp.br>
2025-07-11 19:48:56 -0300

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -1,40 +1,68 @@
 #include <stdio.h>
 
+int verificar_linha(int matriz[9][9]) {
+    for (int i = 0; i < 9; i++) {
+        int freq[10] = {0};
+        for (int j = 0; j < 9; j++) {
+            int num = matriz[i][j];
+            if (num < 1 || num > 9 || freq[num]++) return 0;
+        }
+    }
+    return 1;
+}
 
+int verificar_coluna(int matriz[9][9]) {
+    for (int j = 0; j < 9; j++) {
+        int freq[10] = {0};
+        for (int i = 0; i < 9; i++) {
+            int num = matriz[i][j];
+            if (num < 1 || num > 9 || freq[num]++) return 0;
+        }
+    }
+    return 1;
+}
+
+int verificar_submatriz(int matriz[9][9]) {
+    for (int bloco_i = 0; bloco_i < 9; bloco_i += 3) {
+        for (int bloco_j = 0; bloco_j < 9; bloco_j += 3) {
+            int freq[10] = {0};
+            for (int i = bloco_i; i < bloco_i + 3; i++) {
+                for (int j = bloco_j; j < bloco_j + 3; j++) {
+                    int num = matriz[i][j];
+                    if (num < 1 || num > 9 || freq[num]++) return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
 
 int main() {
-	int i, j;
-	int n=0;
-	int matriz[9][9];
-	int *pmatriz = &matriz[0][0];
-	printf("Digite os elementos da matriz:\n");
-	for (i = 0; i < 9; i++) {
-		for (j = 0; j < 9; j++) {
+    int n;
+    scanf("%d", &n);
 
+    for (int instancia = 1; instancia <= n; instancia++) {
+        int matriz[9][9];
 
-			printf("Elemento [%d][%d]: ", i, j);
-			scanf("%d", &matriz[i][j]);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                scanf("%d", &matriz[i][j]);
+            }
+        }
 
+        int eh_valido = verificar_linha(matriz) &&
+                        verificar_coluna(matriz) &&
+                        verificar_submatriz(matriz);
 
-			while (matriz[i][j] < 0 || matriz[i][j] > 9) {
-				printf("Valor invC!lido. Digite novamente:\n ");
-				scanf("%d", &matriz[i][j]);
-			}
-		}
-	}
+        printf("Instancia %d\n", instancia);
+        if (eh_valido) {
+            printf("SIM\n");
+        } else {
+            printf("NAO\n");
+        }
+        printf("\n"); 
+    }
 
-	for(int i = 0; i < 81; i++) {
-		printf("%d ", *(pmatriz + i));
-		int tamanho = sizeof(matriz) / sizeof(matriz[0][0]);
-	}
-
-
-
-	for (i = 0; i < 9; i++) {
-		for (j = 0; j < 9; j++) {
-			printf("%d ", matriz[i][j]);
-		}
-		printf("\n");
-	}
-	return 0;
+    return 0;
 }
+

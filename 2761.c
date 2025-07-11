082ec9ed547a15b5ee0cd59cc8be700a6c2ae6b6
FelipeCostadeSouza <felipe.souza07@unifesp.br>
2025-07-11 18:47:02 -0300

    Update 2761.c

--- 2761.c
+++ 2761.c
@@ -5,12 +5,13 @@ int main() {
     int A;
     float B;
     char C;
-    char D[49];  
-    if (scanf("%d %f %c", &A, &B, &C) != 3) return 0;
-  
+    char D[51];
+
+    if (scanf("%d %f %c\n", &A, &B, &C) != 3) return 0;
     if (!fgets(D, sizeof D, stdin)) return 0;
-    D[strcspn(D, "\n")] = '\0';  
-    printf("%d %.6f %c %s\n", A, B, C, D);
+    D[strcspn(D, "\n")] = '\0';
+
+    printf("%d%.6f%c%s\n", A, B, C, D);
 
     printf("%d\t%.6f\t%c\t%s\n", A, B, C, D);
 

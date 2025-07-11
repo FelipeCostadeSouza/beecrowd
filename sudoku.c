40f0822f51a42a5e8587542329cf77392a539b6f
FelipeCostadeSouza <felipe.souza07@unifesp.br>
2025-07-11 01:04:35 -0300

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -1,57 +1,40 @@
 #include <stdio.h>
 
+
+
 int main() {
-  int i, j;
-  int n=0;
+	int i, j;
+	int n=0;
+	int matriz[9][9];
+	int *pmatriz = &matriz[0][0];
+	printf("Digite os elementos da matriz:\n");
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
 
-  
 
-  int matriz[9][9];
+			printf("Elemento [%d][%d]: ", i, j);
+			scanf("%d", &matriz[i][j]);
 
-  printf("Digite os elementos da matriz:\n");
-  for (i = 0; i < 9; i++) {
-    for (j = 0; j < 9; j++) {
-      
-       
-       printf("Elemento [%d][%d]: ", i, j);
-       scanf("%d", matriz[i][j]);
-      }
-      
-      while (matriz[i][j] < 0 || matriz[i][j] > 9) {
-        printf("Valor inválido. Digite novamente: ");
-        scanf("%d", matriz[i][j]);
-      }
-    }
-  }
-  for(i=0 && j=0; matriz[i][j+n] != matriz[i][j+1]; j++){
-  
-   if(j<9){
-      matriz[i][2] != matriz[i][j]
-      matriz[i][3] != matriz[i][j]
-      matriz[i][4] != matriz[i][j] 
-      matriz[i][5] != matriz[i][j]      
-      matriz[i][6] != matriz[i][j]
-      matriz[i][7] != matriz[i][j]
-      matriz[i][8] != matriz[i][j]
-   }else{
-    j=0;
-    i++;
-    }
-  }
 
-for (i = 0; i < 9; i++) {
-    for (j = 0; j < 9; j++) {
-      printf("%d ", matriz[i][j]);    
-    }
-    printf("\n");
-  }
-  
+			while (matriz[i][j] < 0 || matriz[i][j] > 9) {
+				printf("Valor invC!lido. Digite novamente:\n ");
+				scanf("%d", &matriz[i][j]);
+			}
+		}
+	}
+
+	for(int i = 0; i < 81; i++) {
+		printf("%d ", *(pmatriz + i));
+		int tamanho = sizeof(matriz) / sizeof(&matriz[0][0]);
+	}
 
 
 
-
-
-
-
-  return 0;
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			printf("%d ", matriz[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
 }

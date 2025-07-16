5d71fd06e70b88bccd6666afed04ef7d52f9444a
FelipeCostadeSouza <felipe.souza07@unifesp.br>
2025-07-15 23:20:24 -0300

    Update 1029.c

--- 1029.c
+++ 1029.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+int calls = 0;
 
-
-void fibonacci(int max, long fib[], long calls[]){
-    fib[0] = 0;
-    fib[1] = 1;
-    calls[0] = 0;
-    calls[1] = 0;
-    for (int n = 2; n <= max; n++) {
-        fib[n] = fib[n - 1] + fib[n - 2];
-        calls[n] = 2 + calls[n - 1] + calls[n - 2];
-    }
+int fib(int n)
+{
+	if (n == 0) return 0;
+	if (n == 1) {
+		return 1;
+	} else {
+		calls+=2;
+		return fib(n-1) + fib(n-2);
+	}
 }
-int main() {
-    int N;
-    if (scanf("%d", &N) != 1) {
-        return 0;
-    }
-    const int Max = 39;
-    long fib[Max + 1], calls[Max + 1];
-    fibonacci(Max, fib, calls);
 
-    while (N--) {
-        int V;
-        scanf("%d", &V);
-        printf("fib(%d) = %ld calls = %ld\n", V, calls[V], fib[V]);
-    }
 
-    return 0;
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	while(n>0) {
+		int x;
+		scanf("%d", &x);
+		calls = 0;
+		int result = fib(x);
+		printf("fib(%d) = %d calls = %d\n", x, calls, result);
+		n--;
+	}
+
+	return 0;
 }
